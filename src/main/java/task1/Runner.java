c4f4e8d60eb60eb99129375c11bf3ba76078e3d9
Luiryna <luiryna322@gmail.com>
2020-04-12 16:05:09 +0300

    first task added

---
+++ src/main/java/task1/Runner.java
@@ -0,0 +1,31 @@
+package task1;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+
+        people.add(new Person("name1", 23));
+        people.add(new Person("name2", 12));
+        people.add(new Person("name3", 56));
+        people.add(new Person("name4", 6));
+        people.add(new Person("name5", 28));
+
+        System.out.println("Compare by age: ");
+
+        people
+                .stream()
+                .sorted(Comparator.comparing(Person::getAge))
+                .forEach(System.out::println);
+
+        System.out.println("Compare by name");
+        
+        people
+                .stream()
+                .sorted(Comparator.comparing(Person::getName))
+                .forEach(System.out::println);
+    }
+}
